9cca01b9818aaa952333e208d67ef95669bb8fd6
ramninder <ramnindersaini.singh@gmail.com>
2022-07-04 18:56:22 -0400

    flux testcase approach3

--- src/test/java/com/ramninder/controller/FluxAndMonoControllerTest.java
+++ src/test/java/com/ramninder/controller/FluxAndMonoControllerTest.java
@@ -6,9 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @WebFluxTest
@@ -43,4 +49,21 @@ public class FluxAndMonoControllerTest {
                 .hasSize(4);
 
     }
+
+    @Test
+    public  void fluxTest_approach3(){
+
+        List<Integer> expectedIntegerList  = Arrays.asList(1,2,3,4);
+
+        EntityExchangeResult<List<Integer>> entityExchangeResult =  webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Integer.class)
+                .returnResult();
+
+        assertEquals(expectedIntegerList, entityExchangeResult.getResponseBody());
+
+    }
+
 }
